e0a9e82ea7a0d8aaba24c8b9800e867a3c6caf04
Erlind <erlind.uxdesigner@gmail.com>
2023-04-26 07:17:48 +0800

    added a feature where console will tell who wins after 5 rounds

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-
+let playerScore = 0;
+let computerScore = 0;
 // loop until 5 rounds
 for (gameRound = 0; gameRound < 5; gameRound++) {
     // player and computer choice
@@ -25,16 +26,22 @@ for (gameRound = 0; gameRound < 5; gameRound++) {
         if (playerSelection === computerSelection) {
             return `You selected ${playerSelection}. Computer selected ${computerSelection}. It's a tie.`
         } else if (playerSelection === "rock" && computerSelection === "paper"){
+            computerScore++;
             return `You selected ${playerSelection}. Computer selected ${computerSelection}. You lose.`
         } else if (playerSelection === "rock" && computerSelection === "scissors") {
+            playerScore++;
             return `You selected ${playerSelection}. Computer selected ${computerSelection}. You Win.`
         } else if (playerSelection === "paper" && computerSelection === "rock") {
+            playerScore++;
             return `You selected ${playerSelection}. Computer selected ${computerSelection}. You Win.`
         } else if (playerSelection === "paper" && computerSelection === "scissors") {
+            computerScore++;
             return `You selected ${playerSelection}. Computer selected ${computerSelection}. You lose.`
         } else if (playerSelection === "scissors" && computerSelection === "rock") {
+            computerScore++;
             return `You selected ${playerSelection}. Computer selected ${computerSelection}. You lose.`
         } else if (playerSelection === "scissors" && computerSelection === "paper") {
+            playerScore++;
             return `You selected ${playerSelection}. Computer selected ${computerSelection}. You win.`
         }
     }
@@ -42,6 +49,16 @@ for (gameRound = 0; gameRound < 5; gameRound++) {
     // print in the console the roun result
     console.log(game(playerSelection, computerSelection));
 }
+
+let whoWin;
+if (playerScore > computerScore) {
+    whoWin = "Congratulations, you win!";
+} else if (playerScore < computerScore) {
+    whoWin = "Too bad, computer wins";
+} else {
+    whoWin = "It's a tie. Try to play another game."
+}
+console.log(`After 5 rounds. Player: ${playerScore}. Computer: ${computerScore}. ${whoWin}`)
 
 
 
